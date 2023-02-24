5596f10025122de9f5e5dab5a3371b1d1d0bc4fe
Madhumita Mondal <48853024+madhumita-mondal@users.noreply.github.com>
2023-02-23 20:51:09 -0800

    Add files via upload

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -7,17 +7,3 @@ const errorHandler = (err, req, res, next) => {
     });
   };
   module.exports = { errorHandler };
-
-// const errorHandler = (err, req, res, next) => {
-//   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-//   res.status(statusCode);
-//   res.setHeader('Access-Control-Allow-Origin', 'https://eshophy-frontend.onrender.com');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   res.json({
-//     message: err.message,
-//     stack: process.env.NODE_ENV === "production" ? null : err.stack,
-//   });
-// };
-
-// module.exports = { errorHandler };
