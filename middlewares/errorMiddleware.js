c9b6fe6a6992f1ccaf2b85db137117be98ceedff
Madhumita Mondal <48853024+madhumita-mondal@users.noreply.github.com>
2023-02-23 20:31:25 -0800

    Update errorMiddleware.js

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,23 +1,23 @@
-// const errorHandler = (err, req, res, next) => {
-//     const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-//     res.status(statusCode);
-//     res.json({
-//       message: err.message,
-//       stack: process.env.NODE_ENV === "production" ? null : err.stack,
-//     });
-//   };
-//   module.exports = { errorHandler };
-
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.setHeader('Access-Control-Allow-Origin', 'https://eshophy-frontend.onrender.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
-};
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+      message: err.message,
+      stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+  };
+  module.exports = { errorHandler };
 
-module.exports = { errorHandler };
+// const errorHandler = (err, req, res, next) => {
+//   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+//   res.status(statusCode);
+//   res.setHeader('Access-Control-Allow-Origin', 'https://eshophy-frontend.onrender.com');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   res.json({
+//     message: err.message,
+//     stack: process.env.NODE_ENV === "production" ? null : err.stack,
+//   });
+// };
+
+// module.exports = { errorHandler };
